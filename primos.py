9374ff94c62ae84182a5d8b79bed8fd5171b50d3
thairoMatheus <105610057+thairoMatheus@users.noreply.github.com>
2022-05-15 20:22:24 -0300

    Add files via upload

--- primos.py
+++ primos.py
@@ -1,13 +1,15 @@
 import argparse
 
 def primo(n):
-    if n == 0 or n == 1:
+    if n == 2:
+        return True
+    if n == 0 or n == 1 or n%2==0:
         return False
-    for i in range(2,int(n**0.5+1)):
+    for i in range(3,int(n**0.5+1),2):
         if (n%i == 0):
             return False
     return True
-    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
